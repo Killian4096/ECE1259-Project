f46bcdc5bdb17f1e3f44a4161eaa00f58c0f5f8f
JLipnerPitt <122459494+JLipnerPitt@users.noreply.github.com>
2023-04-03 11:21:12 -0400

    Add files via upload

--- main.m
+++ main.m
@@ -1,17 +1,15 @@
 clc
 j = 1i;
-dielectric_constants = containers.Map([1, 2, 3, 4, 5],... 
-{'Air, εᵣ = 1.00', 'Teflon, εᵣ = 2.10', 'Polyethylene, εᵣ = 2.26',... 
- 'Mica, εᵣ = 5.40', 'Glass, εᵣ = 10.0'});
-relative_permitivity = [1, 2.1, 2.26, 5.4, 10];
-dielectric_strength = [3e6, 60e6, 47e6, 200e6, 30e6];
-conductivity = [0, 1e-15, 1e-16, 1e-15, 1e-12];
+dielectric_constants = containers.Map([1, 2, 3, 4, 5, 6],...
+{'Air, εᵣ = 1.00', 'Teflon, εᵣ = 2.10', 'Polystyrene, εᵣ = 2.56',... 
+ 'Quartz(fused), εᵣ = 3.8', 'Mica, εᵣ = 5.40', 'Glass, εᵣ = 10.0'});
+relative_permitivity = [1, 2.1, 2.56, 3.8, 5.4, 10];
+dielectric_strength = [3e6, 60e6, 20e6, 30e6, 200e6, 30e6];
+conductivity = [0, 10e-15, 10e-17, 10e-17, 10e-15, 10e-12];
 
 %calling functions
-GraphLossTangent(1e-12, 10, 143e-9);
 displayDielectrics(dielectric_constants);
 [caps, bdvs] = inputScript(relative_permitivity, dielectric_strength);
-Display(caps, bdvs);
 
 function displayDielectrics(dielectric_constants)
     %displays list of dielectrics and their permitivities
@@ -26,20 +24,20 @@ function [caps, bdvs] = inputScript(relative_permitivity, dielectric_strength)
     bdvs = []; % stores all break down voltages from the user created capacitors
     flag = true; % checks if user still wants to design capacitors
     while(flag == true)
-        userInput = input("Select your dielectric(number), choose the area of the capacitor's plates(cm), and the distance between the plates(mm):\n", 's');
+        userInput = input("Select your dielectric(number), choose the area of the capacitor's plates(m), and the distance between the plates(mm):\n", 's');
         data = regexp(userInput, '\d+(\.\d+)?', 'match'); % removes common unwanted inputs
         values = str2double(data);
         while(length(values) ~= 3) % error checking for invalid input
             if (length(values)<3)
                 fprintf("Invalid input. Only enter numbers separated by some delimiter.\n");
                 displayDielectrics(dielectric_constants);
-                userInput = input("Select your dielectric(number), choose the area of the capacitor's plates(cm), and the distance between the plates(mm):\n", 's');
+                userInput = input("Select your dielectric(number), choose the area of the capacitor's plates(m), and the distance between the plates(mm):\n", 's');
                 data = regexp(userInput, '\d+(\.\d+)?', 'match');
                 values = str2double(data);
             else
                 fprintf("Too many inputs. Only enter numbers corresponding to the dielectric, area, and separation_distance.\n");
                 displayDielectrics(dielectric_constants);
-                userInput = input("Select your dielectric(number), choose the area of the capacitor's plates(cm), and the distance between the plates(mm):\n", 's');
+                userInput = input("Select your dielectric(number), choose the area of the capacitor's plates(m), and the distance between the plates(mm):\n", 's');
                 data = regexp(userInput, '\d+(\.\d+)?', 'match');
                 values = str2double(data);
             end
@@ -52,7 +50,8 @@ function [caps, bdvs] = inputScript(relative_permitivity, dielectric_strength)
         capacitance = Capacitance(dielectric, area, distance);
         caps(end+1) = capacitance; % adds 'n' capacitance to caps list
         bdv = BreakdownVoltage(dielectric_strength(values(1)), distance);
-        bdvs(end+1) = bdv; % adds 'n' breakdown voltage to bdvs list 
+        bdvs(end+1) = bdv; % adds 'n' breakdown voltage to bdvs list
+        GraphLosses(1e-12, dielectric, capacitance*10^-9);
         userInput = input("Would you like to create another capacitor(Y/N)? ", 's');
         if (strcmpi(userInput, 'Y'))
             flag = true;
@@ -75,23 +74,33 @@ function bdv = BreakdownVoltage(E, d)
     fprintf('V_BR = %f V\n', bdv);
 end
 
-function GraphLossTangent(conductivity, relative_perm, capacitance)
+function GraphLosses(conductivity, relative_perm, capacitance)
     p = 8.854*10^-12*relative_perm; % permitivity
     
     subplot(2, 1, 1);
-    x = linspace(0, 100, 100);
-    y1 = tan(conductivity./(x*10^6*p));
-    plot(x, y1)
+    %x = linspace(0, 100, 100);
+    %y1 = tan(conductivity./(x*10^6*p));
+    [capacitance, conductivity]
+    sys1 = tf([capacitance, conductivity],1)
+    bodeplot(sys1)
     xlabel('Frequency(Mhz)');
     ylabel('Loss Tangent');
     
     subplot(2, 1, 2);
     x = linspace(0, 100, 100);
     y2 = 1./(x*capacitance);
-    plot(x, y2)
+    plot(x, y2, 'r')
     xlabel('Frequency(Mhz)');
     ylabel('Capacitive Reactance');
     
+    %{
+    subplot(2, 1, 3);
+    x = linspace(0, 100, 100);
+    y3 = 1./(x*capacitancev);
+    loglog(x, y3, 'r')
+    xlabel('Frequency(Mhz)');
+    ylabel('Leakage Current');
+    %}
 end
 
 
